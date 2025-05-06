388e2d1351e2b7cc6d07fa55745355d4bd27f899
Maximiliano Garcia Roe <mrgarcia1@miuandes.cl>
2025-05-06 00:03:00 -0400

    feat: add form authors

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,4 +6,43 @@ class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
   end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    
+    if @book.save
+      redirect_to books_path, notice: 'Book was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book), notice: 'Book was successfully updated.'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path, notice: 'Book was successfully deleted.'
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :year, :editorial, :author_id)
+  end
 end
