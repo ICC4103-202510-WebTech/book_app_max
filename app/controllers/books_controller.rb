351a67a2284a7789ac2920271263a0913803ccea
Maximiliano Garcia Roe <mrgarcia1@miuandes.cl>
2025-05-26 23:07:56 -0400

    refactor: clean linters

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -13,9 +13,9 @@ class BooksController < ApplicationController
 
   def create
     @book = Book.new(book_params)
-    
+
     if @book.save
-      redirect_to books_path, notice: 'Book was successfully created.'
+      redirect_to books_path, notice: "Book was successfully created."
     else
       render :new, status: :unprocessable_entity
     end
@@ -28,7 +28,7 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
-      redirect_to book_path(@book), notice: 'Book was successfully updated.'
+      redirect_to book_path(@book), notice: "Book was successfully updated."
     else
       render :edit, status: :unprocessable_entity
     end
@@ -37,7 +37,7 @@ class BooksController < ApplicationController
   def destroy
     @book = Book.find(params[:id])
     @book.destroy
-    redirect_to books_path, notice: 'Book was successfully deleted.'
+    redirect_to books_path, notice: "Book was successfully deleted."
   end
 
   private
--- app/models/author.rb
+++ app/models/author.rb
@@ -1,7 +1,7 @@
 class Author < ApplicationRecord
   has_many :books
   has_rich_text :description
-  
+
   validates :first_name, presence: { message: "no puede estar en blanco" }
   validates :last_name, presence: { message: "no puede estar en blanco" }
   validates :birth, numericality: { only_integer: true, greater_than: 0, allow_nil: true, message: "debe ser un número positivo" }
